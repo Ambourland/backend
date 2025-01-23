455fc135b6fadb6725fa19c48d2434ee5579ef2a
Ambourland <144298214+Ambourland@users.noreply.github.com>
2025-01-22 18:27:04 -0600

    test

--- week01/callMeBacks/functions.js
+++ week01/callMeBacks/functions.js
@@ -15,20 +15,51 @@ const first = function (arr, index) {
  return arr.slice(0,index)
 };
   
-const last = function () {
-  
+const last = function (arr, index = 0) {
+  if (index === 0) {
+    return [];
+  }
+  if (index >= arr.length) {
+    return arr;
+  }
+  return arr.slice(arr.length - index);
 };
 
-const each = function () {
-  
+const each = function (animals, callback) {
+  if (Array.isArray(animals)) {
+    for (let i = 0; i < animals.length; i++) {
+      callback(animals[i], i, animals);
+    }
+  }else if (typeof animals === 'object' && animals !== null) {
+    for (let key in animals) {
+      if (animals.hasOwnProerty(key)) {
+        callback(animals[key], key, animals)
+      }
+    }
+  }
 };
 
-const indexOf = function () {
-  
+const indexOf = function(arr, searchElement, fromIndex = 0) {
+  if (fromIndex < 0) {
+    fromIndex = Math.max(arr.length + fromIndex, 0);
+  }
+
+  for (let i = fromIndex; i < arr.length; i++) {
+    if (arr[i] === searchElement) {
+      return i; 
+    }
+  }
+
+  return -1; 
 };
 
-const map = function () {
+const map = function (arr, callback) {
+  const result = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callback(arr[i], i, arr))
+  }
+  return result
 }
 
 const filter = (collection, callback)=>{
